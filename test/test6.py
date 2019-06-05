4be510307ac98a7a149d31449d77178d74fbf9aa
frank <frank.chang@shoufuyou.com>
2019-06-05 15:58:15 +0800

    update .gitignore

---
+++ test/test6.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2019/6/5 15:46
+@File    : test6.py
+@Author  : frank.chang@shoufuyou.com
+"""
+
+
+import flask
+
+from    flask import request
+
+from flask import Flask
+if __name__ == '__main__':
+    pass
+    
